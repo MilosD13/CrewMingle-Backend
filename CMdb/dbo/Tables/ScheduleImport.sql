0969e25abbf1da1d3e0458abc0746191539fca3a
bims <little_anjh@yahoo.com>
2024-10-16 06:41:01 +1100

    Upload Table Added table to upload cruisedata

---
+++ CMdb/dbo/Tables/ScheduleImport.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ScheduleImport]
+(
+	[CruiselineId] INT NOT NULL PRIMARY KEY,
+	[Cruiseline] NVARCHAR(255) NOT NULL,
+	[ShipId] INT NOT NULL,
+	[Ship] NVARCHAR(255) NOT NULL,
+	[JourneyId] INT NOT NULL,
+	[StartDate] DATE NOT NULL,
+	[Description] NVARCHAR(255) NOT NULL,
+	[Date] DATE NOT NULL,
+	[ArrivalTime] NVARCHAR(16) NULL,
+	[DepartureTime] NVARCHAR(16) NULL,
+	[Port] NVARCHAR(255) NOT NULL
+
+)
