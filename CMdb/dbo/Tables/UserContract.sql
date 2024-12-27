baaa2d7332dc9a5d3ad804ca7cc8c373862096c9
MilosD13 <milosdokic13@gmail.com>
2024-12-27 19:11:41 +1100

    CM-69-Add user contract table

---
+++ CMdb/dbo/Tables/UserContract.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[UserContract]
+(
+    [Id] UNIQUEIDENTIFIER  DEFAULT NEWID()      PRIMARY KEY,
+    [CreatedDate] DATETIME2(7)    NOT NULL      DEFAULT GETUTCDATE(),
+    [UserAccountId] UNIQUEIDENTIFIER NOT NULL,
+    [ShipId] INT NOT NULL,
+    [StartDate] DATETIME2(7) NOT NULL,
+    [EndDate] DATETIME2(7) NOT NULL,
+    [IsDeleted] BIT NOT NULL DEFAULT 0,
+
+    CONSTRAINT FK_UserContract_UserAccount
+        FOREIGN KEY ([UserAccountId]) 
+        REFERENCES [dbo].[UserAccount]([Id]),
+
+    CONSTRAINT FK_UserContract_CruiseShip
+        FOREIGN KEY ([ShipId]) 
+        REFERENCES [dbo].[CruiseShip]([Id])
+);
