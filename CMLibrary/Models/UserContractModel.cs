46a600a00fe0c21871ddccd5a78aed9bc8486c23
MilosD13 <milosdokic13@gmail.com>
2024-12-27 19:10:35 +1100

    CM-122-Add contract model

---
+++ CMLibrary/Models/UserContractModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CMLibrary.Models;
+public class UserContractModel
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Guid UserAccountId { get; set; }
+    public int? ShipId { get; set; }          // optional
+    public DateTime? StartDate { get; set; }  // optional
+    public DateTime? EndDate { get; set; }    // optional
+    public bool IsDeleted { get; set; }
+}
