d707ee8e1cf93996f5351f0bf075cf45c7f45b0f
MilosD13 <milosdokic13@gmail.com>
2024-12-29 13:50:40 +1100

    CM-134: Create Crusiline, Ship, ShipSchedule and ShipItinerary Model

---
+++ CMLibrary/Models/ShipItineraryFlatModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CMLibrary.Models;
+
+public class ShipItineraryFlatModel
+{
+    public string Cruiseline { get; set; } = string.Empty;
+    public string Ship { get; set; } = string.Empty;
+    public int ShipId { get; set; }
+    public int PortId { get; set; }
+    public string PortName { get; set; } = string.Empty;
+    public string PortCountry { get; set; } = string.Empty;
+    public decimal? Latitude { get; set; }
+    public decimal? Longitude { get; set; }
+
+    public DateTime? ArrivalDate { get; set; }
+    public TimeSpan? ArrivalTime { get; set; }
+    public DateTime? DepartureDate { get; set; }
+    public TimeSpan? DepartureTime { get; set; }
+    public int? RowNum { get; set; }
+}
---
+++ CMLibrary/Models/ShipModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CMLibrary.Models;
+public class ShipModel
+{
+    public int ShipId { get; set; }
+    public string ShipName { get; set; } = string.Empty;
+    public int CruiselineId { get; set; }
+    public string CruiselineName { get; set; } = string.Empty;
+
+    // The itinerary or schedule
+    public List<ShipScheduleModel> ShipSchedules { get; set; } = new List<ShipScheduleModel>();
+}
