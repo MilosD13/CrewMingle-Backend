40b318f84e460750c7339cffd30e36119da16931
MilosD13 <milosdokic13@gmail.com>
2024-12-29 14:33:25 +1100

    Code cleanup

--- CMLibrary/DataAccess/ISqlDataAccess.cs
+++ CMLibrary/DataAccess/ISqlDataAccess.cs
@@ -1,9 +1,7 @@
-﻿
-namespace CMLibrary.DataAccess
+﻿namespace CMLibrary.DataAccess;
+
+public interface ISqlDataAccess
 {
-    public interface ISqlDataAccess
-    {
-        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
-        Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
-    }
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
 }
